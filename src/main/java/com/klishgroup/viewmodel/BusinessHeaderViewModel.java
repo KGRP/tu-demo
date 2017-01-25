033ce8a8f656a58eb28f505aad42cf9f54ae15b0
rpatel <rpatel@klishgroup.com>
2017-01-25 14:04:16 -0500

    more updates

--- src/main/java/com/klishgroup/viewmodel/BusinessHeaderViewModel.java
+++ src/main/java/com/klishgroup/viewmodel/BusinessHeaderViewModel.java
@@ -1,13 +1,17 @@
 package com.klishgroup.viewmodel;
 
 import com.klishgroup.model.component.BusinessHeader;
+import com.klishgroup.model.page.IndustryPage;
 import com.klishgroup.view.base.util.LinkView;
 import com.klishgroup.view.component.BusinessHeaderMainNavView;
 import com.klishgroup.view.component.MainNavNodeView;
 import com.klishgroup.view.component.MainNavSubNavView;
 import com.psddev.cms.db.Seo;
+import com.psddev.dari.db.Query;
+import com.psddev.dari.util.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -20,12 +24,12 @@ public class BusinessHeaderViewModel extends AbstractHeaderViewModel<BusinessHea
 
         mainNav.solutionsAndProducts(new MainNavNodeView.Builder()
                 .title(model.getSollutionsAndProducts().getLabel())
-                .id(StringUtils.remove(StringUtils.capitalize(model.getSollutionsAndProducts().getLabel()), " "))
+                .id(com.psddev.dari.util.StringUtils.toCamelCase(model.getSollutionsAndProducts().getLabel()))
                 .addAllToSubNavs(model.getSollutionsAndProducts().getSubNavs().stream()
                         .filter(Objects::nonNull)
                         .map(subNav -> new MainNavSubNavView.Builder()
                                 .title(subNav.getTitle())
-                                .id(StringUtils.remove(StringUtils.capitalize(subNav.getTitle()), " "))
+                                .id(com.psddev.dari.util.StringUtils.toCamelCase(subNav.getTitle()))
                                 .addAllToLinks(subNav.getPages().stream()
                                         .filter(Objects::nonNull)
                                         .map(page -> new LinkView.Builder()
@@ -42,16 +46,28 @@ public class BusinessHeaderViewModel extends AbstractHeaderViewModel<BusinessHea
                         .collect(Collectors.toList()))
                 .build());
 
-        mainNav.industries(new MainNavNodeView.Builder()
-                .title(model.getIndustries().getLabel())
-                .id(StringUtils.remove(StringUtils.capitalize(model.getIndustries().getLabel()), " "))
-                .build());
+        List<IndustryPage> industries = Query.from(IndustryPage.class).selectAll();
+        if (!ObjectUtils.isBlank(industries)) {
+            mainNav.industries(new MainNavNodeView.Builder()
+                    .title(model.getIndustries().getLabel())
+                    .id(StringUtils.remove(StringUtils.capitalize(model.getIndustries().getLabel()), " "))
+                    .addAllToLinks(industries.stream()
+                            .filter(Objects::nonNull)
+                            .map(page -> new LinkView.Builder()
+                                    .href(page.getPermalink())
+                                    .body(page.getName())
+                                    .description(page.as(Seo.ObjectModification.class).getDescription())
+                                    .index(org.apache.commons.lang.StringUtils.remove(org.apache.commons.lang.StringUtils.capitalize(page.getName() + page.getId().toString()), " "))
+                                    .id(org.apache.commons.lang.StringUtils.remove(org.apache.commons.lang.StringUtils.capitalize(page.getName()), " "))
+                                    .build())
+                            .collect(Collectors.toList()))
+                    .build());
+        }
 
         mainNav.insightsAndEvents(new MainNavNodeView.Builder()
                 .title(model.getInsightsAndEvents().getLabel())
                 .id(StringUtils.remove(StringUtils.capitalize(model.getInsightsAndEvents().getLabel()), " "))
-                .content(createView(AbstractViewModel.MODULE_VIEW_TYPE,
-                        model.getInsightsAndEvents().getLinks()))
+                .content(createView(HeaderInsightsAndEventsViewModel.class, model.getInsightsAndEvents().getLinks()))
                 .build());
 
         return mainNav.build();
---
+++ src/main/java/com/klishgroup/viewmodel/HeaderInsightsAndEventsViewModel.java
@@ -0,0 +1,55 @@
+package com.klishgroup.viewmodel;
+
+import com.klishgroup.model.InsightOrEvent;
+import com.klishgroup.model.Link;
+import com.klishgroup.model.component.InsightsAndEvents;
+import com.klishgroup.view.base.util.ConcatenatedView;
+import com.klishgroup.view.base.util.LinkView;
+import com.klishgroup.view.component.HeaderInsightsAndEventsView;
+import com.psddev.dari.db.Query;
+import com.psddev.dari.util.ObjectUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HeaderInsightsAndEventsViewModel extends AbstractViewModel<InsightsAndEvents> implements HeaderInsightsAndEventsView {
+
+    @Override
+    public Object getTitle() {
+        return model.getTitle();
+    }
+
+    @Override
+    public Object getViewAll() {
+
+        Link viewAllLink = model.getViewAll();
+
+        return new LinkView.Builder()
+                .href(viewAllLink.getHref())
+                .body(viewAllLink.getText())
+                .build();
+    }
+
+    @Override
+    public Object getContent() {
+
+        List<InsightOrEvent> items = Query.from(InsightOrEvent.class).sortDescending("cms.content.publishDate").selectAll();
+
+        if (ObjectUtils.isBlank(items)) {
+            if (ObjectUtils.isBlank(model.getDefaultInsightOrEvent())) {
+                return null;
+            }
+            items = new ArrayList<>();
+            items.add(model.getDefaultInsightOrEvent());
+        }
+        return new ConcatenatedView.Builder()
+                .addAllToItems(items.stream()
+                        .filter(Objects::nonNull)
+                        .map(item -> createView(AbstractViewModel.MODULE_VIEW_TYPE, item))
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
